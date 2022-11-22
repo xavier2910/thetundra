14d789df30a34dea167227ec5f71d57d4e8f7e4e
xavier2910 <x.distelzweig@gmail.com>
2022-11-22 16:46:08 -0600

    Added Location and Direction data types

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,12 @@ module Lib
     ( playGame
     ) where
 
+import qualified Data.Map as Map
+
+data Direction = N | S | E | W | NE | NW | SE | SW
+data Location = Location { description :: String
+                         , connections :: Map.Map Direction Location }
+
 playGame :: IO ()
 playGame = do putStrLn "The Tundra v1.0.0"
 
