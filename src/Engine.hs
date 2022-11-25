4f94fdcc5f75aefe92f62eb1106e9b7c1c9f9410
xavier2910 <x.distelzweig@gmail.com>
2022-11-24 21:04:33 -0600

    created world types

--- src/Engine.hs
+++ src/Engine.hs
@@ -1,1 +1,27 @@
-module Engine where
+module Engine ( Location
+              , Vehicle
+              , Furniture
+              , NPC
+              , Role(defaultInventory)
+              , Item
+              , Inventory) where
+
+
+data Location = Location { lDescription :: String
+                         , isPlayerHere :: Bool
+                         , children :: Inventory } deriving(Show, Eq)
+
+data Vehicle = Vehicle { vDescription :: String 
+                       , passengers :: Inventory } deriving(Show, Eq)
+newtype Furniture = Furniture { fDescription :: String } deriving(Show, Eq)
+
+data NPC r =  NPC { name :: String
+                  , role :: r
+                  , nDescription :: String
+                  , nInventory :: Inventory } deriving(Show, Eq)
+
+class (Show r) => Role r where
+    defaultInventory :: r -> Inventory
+
+newtype Item = Item { iDescription :: String } deriving(Show, Eq)
+type Inventory = [Item]
