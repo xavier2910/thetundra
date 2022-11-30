1d2aeb006e9fd5c98979cd16ab624d70133b8e30
xavier2910 <x.distelzweig@gmail.com>
2022-11-29 20:44:50 -0600

    removed older types; created non-binary tree to use to track locations and their connections

--- src/Engine.hs
+++ src/Engine.hs
@@ -1,54 +1,10 @@
-module Engine ( Location ( lDescription
-                         , isPlayerAt
-                         , inhabitants
-                         , getConnectedLocations )
-              , Inhabitants
-              , Direction (..)
-              , Vehicle ( vDescription
-                        , passengers )
-              , Furniture (fDescription)
-              , NPC ( name
-                    , role
-                    , nDescription
-                    , nInventory )
-              , Role (defaultInventory)
-              , Item
-              , Inventory) where 
+module Engine ( ) where 
 
-import qualified Data.Map as M 
-
--- | represents a location and its attached locations; therefore the whole world by recursion
-data Location r = Location { lDescription :: String
-                           , isPlayerAt :: Bool
-                           , inhabitants :: Inhabitants r
-                           , getConnectedLocations :: M.Map Direction (Location r) } deriving(Eq)
-
-type Inhabitants r = ([Furniture], [Vehicle], [NPC r], Inventory)
-
-data Direction = N | S |E | W | NE | NW | SE | SW deriving(Show, Eq)
-
-newtype Furniture = Furniture { fDescription :: String } deriving(Show, Eq)
-
-data Vehicle = Vehicle { vDescription :: String 
-                       , passengers :: Inventory } deriving(Show, Eq)
-
-data NPC r =  NPC { name :: String
-                  , role :: r
-                  , nDescription :: String
-                  , nInventory :: Inventory } deriving(Show, Eq)
-
-class (Show r) => Role r where
-    defaultInventory :: r -> Inventory
-
-newtype Item = Item { iDescription :: String } deriving(Show, Eq)
-
-type Inventory = [Item]
+import qualified Data.Map as M
 
 
+data Direction = N | S | E | W | NE | NW | SE | SW deriving(Show, Eq)
 
-instance (Role r) => Show (Location r) where
-    show loc = "Location { lDescription = " ++ show (lDescription loc) 
-                     ++ ", isPlayerAt = " ++ show (isPlayerAt loc)
-                     ++ ", inhabitants = " ++ show (inhabitants loc)
-                     ++ ", getConnectedLocations = fromList " ++ show (M.toList $ getConnectedLocations loc)
-
+-- | Non-binary tree. Never empty. Uses a Data.Map.Map to keep track of its children.
+data Tree k v = Leaf v 
+              | Node v (M.Map k v)
