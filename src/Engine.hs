19a81f3faf046b0ad63ce5235d283dc968e4e241
xavier2910 <x.distelzweig@gmail.com>
2022-12-06 20:42:15 -0600

    got cardinal direction cmds functioning

--- src/Engine.hs
+++ src/Engine.hs
@@ -5,10 +5,11 @@ module Engine ( Direction(..)
 import qualified Data.Map as M
 
 
-data Direction = N | S | E | W | NE | NW | SE | SW deriving(Show, Eq, Ord)
+data Direction = N | S | E | W | NE | NW | SE | SW deriving(Show, Read, Eq, Ord)
 
 -- | Non-binary tree. Never empty. Uses a Data.Map.Map to keep track of its children.
-data Tree k v = Leaf v 
-              | Node v (M.Map k (Tree k v))
+data Tree k v = Leaf { value :: v } 
+              | Node { value :: v
+                     , children :: M.Map k (Tree k v) }
 
 data Location = Location { lDescription :: String }
--- src/Player.hs
+++ src/Player.hs
@@ -3,14 +3,33 @@ module Player
     ) where
 
 import Story ( start )
-import Engine ( Location, Tree, Direction )
+import Engine ( Location(Location), Tree(children, value), Direction )
+
+import Data.Char ( toUpper )
+import qualified Data.Map as M
+import System.IO (stdout, hFlush)
+
 
 begin :: IO ()
 begin = do putStrLn "The Tundra v0.1.0"
+           let msg = case value start of Location message -> message
+                                   
+           putStrLn $ "\n" ++ msg ++ "\n"
            playGame start
 
 playGame :: Tree Direction Location -> IO ()
-playGame st = putStrLn "todo"
+playGame st = do putStr "> "
+                 hFlush stdout
+                 command <- getLine
+
+                 -- n. b. this does not check against Leaf's
+                 case reads $ map toUpper command :: [(Direction, String)] of [(dir, _)] -> case M.lookup dir $ children st of Just tree -> do let msg = case value tree of Location message -> message
+                                                                                                                                               putStrLn $ "\n" ++ msg ++ "\n"
+                                                                                                                                               playGame tree
+                                                                                                                               Nothing -> do putStrLn "\nYou can't go that way :(\n"
+                                                                                                                                             playGame st
+                                                                              _ -> do putStrLn "\nBad command :(\n"
+                                                                                      playGame st
               
 
 
