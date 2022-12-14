d315dcaca0d10e57c740aecfa22bcac40c149c34
xavier2910 <x.distelzweig@gmail.com>
2022-12-13 20:24:31 -0600

    formatted Player for readablility

--- src/Player.hs
+++ src/Player.hs
@@ -11,25 +11,37 @@ import System.IO (stdout, hFlush)
 
 
 begin :: IO ()
-begin = do putStrLn "The Tundra v0.1.0"
-           let msg = case value start of Location message -> message
-                                   
-           putStrLn $ "\n" ++ msg ++ "\n"
-           playGame start
+begin = do 
+    putStrLn "The Tundra v0.1.0"
+    let msg = case value start of Location message -> message
+                            
+    putStrLn $ "\n" ++ msg ++ "\n"
+    playGame start
 
 playGame :: Tree Direction Location -> IO ()
-playGame st = do putStr "> "
-                 hFlush stdout
-                 command <- getLine
+playGame st = do 
+    putStr "> "
+    hFlush stdout
+    command <- getLine
 
-                 -- n. b. this does not check against Leaf's
-                 case reads $ map toUpper command :: [(Direction, String)] of [(dir, _)] -> case M.lookup dir $ children st of Just tree -> do let msg = case value tree of Location message -> message
-                                                                                                                                               putStrLn $ "\n" ++ msg ++ "\n"
-                                                                                                                                               playGame tree
-                                                                                                                               Nothing -> do putStrLn "\nYou can't go that way :(\n"
-                                                                                                                                             playGame st
-                                                                              _ -> do putStrLn "\nBad command :(\n"
-                                                                                      playGame st
+    -- n. b. this does not check against Leaf's
+    case reads $ map toUpper command :: [(Direction, String)] of 
+
+        [(dir, _)] -> case M.lookup dir $ children st of -- n. b errors on leaf
+
+            Just tree -> do
+                let msg = case value tree of (Location message) -> message
+                putStrLn $ "\n" ++ msg ++ "\n"
+                playGame tree
+
+            Nothing -> do 
+                putStrLn "\nYou can't go that way :(\n"
+                playGame st
+
+
+        _ -> do 
+            putStrLn "\nBad command :(\n"
+            playGame st
               
 
 
